1e0f4bc6ef1467bc85848bac07add7427681e2d6
katie-b9030 <111714611+katie-b9030@users.noreply.github.com>
2024-02-25 21:00:08 -0500

    now done

--- bogart-k-pe07/web-audio-hw-3/src/visualizer.js
+++ bogart-k-pe07/web-audio-hw-3/src/visualizer.js
@@ -125,7 +125,8 @@ const draw = (params = {}) => {
 			// data[i+2] is the blue channel
 			// data[i+3] is the alpha channel
 			data[i] = data[i + 1] = data[i + 2] = 0; // zero out the red and green and blue channels
-			data[i] = 255; // make the red channel 100% red
+			// data[i] = 255; // make the red channel 100% red
+			data[i+1] = data[i+2] = 125
 		} // end if
 
 		// invert?
