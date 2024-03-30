c2c2c2e1fd3adc35b5e409005ca355c46ae36862
katie-b9030 <111714611+katie-b9030@users.noreply.github.com>
2024-03-30 12:30:59 -0400

    finished typescript

--- bogart-k-ts-practice/bogart-k-ts-practice.ts
+++ bogart-k-ts-practice/bogart-k-ts-practice.ts
@@ -5,47 +5,50 @@
   Then strongly type the `todoItems` array
 */
 interface TodoItem {
-    
+  id: number
+  title: string
+  status: Status
+  completedOn?: Date
 }
 
 /*
-  #2 - Strongly type the `status` property with an enum
-  Note the `status` values below: "done", "in-progess" etc
+#2 - Strongly type the `status` property with an enum
+Note the `status` values below: "done", "in-progess" etc
 */
 enum Status{
-    done = "done",
-    inProgress = "in-progess",
-    todo = "todo"
+  Done = "done",
+  InProgress = "in-progess",
+  Todo = "todo"
 }
 
 /*
-  #3 - Strongly type the parameters and return values of `addTodoItem()` and `getNextId()`
+#3 - Strongly type the parameters and return values of `addTodoItem()` and `getNextId()`
 */
 
 // **When you are done, there must not be any errors under the Playground's "Errors" tab**
 
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: Status.done, completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: Status.inProgress },
-    { id: 3, title: "Write the best web app in the world", status: Status.todo },
+const todoItems: TodoItem[] = [
+  { id: 1, title: "Learn HTML", status: Status.Done, completedOn: new Date("2021-09-11") },
+  { id: 2, title: "Learn TypeScript", status: Status.InProgress },
+  { id: 3, title: "Write the best web app in the world", status: Status.Todo },
 ]
 
-function addTodoItem(todo: string) {
-    const id = getNextId(todoItems);
+function addTodoItem(todo: string): TodoItem {
+  const id = getNextId(todoItems);
 
-    const newTodo = {
-        id,
-        title: todo,
-        status: Status.todo,
-    }
+  const newTodo = {
+      id,
+      title: todo,
+      status: Status.Todo,
+  }
 
-    todoItems.push(newTodo)
+  todoItems.push(newTodo)
 
-    return newTodo
+  return newTodo
 }
 
-function getNextId(items: Array<number>) {
-    return items.reduce((max: number, x: number) => x.id > max ? x.id : max, 0) + 1;
+function getNextId<T extends { id:number }>(items: T[]): number {
+  return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1;
 }
 
 const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
